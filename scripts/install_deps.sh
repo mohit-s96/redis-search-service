73f893c0f81f90c12cb997feed4bf44783f00648
msx47 <mohitsrivastava67@gmail.com>
2022-01-22 22:30:58 +0530

    fix: source bach profile to deploy session

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /home/ec2-user/apps/redis-search-service
 yarn
 yarn build
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /home/ec2-user/apps/redis-search-service
 source /usr/local/env/dotenv.env
 pm2 start dist/index.js
