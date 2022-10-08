672c8eb45824cd49550614c9799a13f4f1fc26ec
Mohit Srivastava <mohitsrivastava67@gmail.com>
2022-10-08 17:08:04 +0530

    #1: add node env as production for prod deployment

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/apps/redis-search-service
 source /usr/local/env/dotenv.env
 pm2 stop all
 pm2 delete index
-pm2 start --node-args="--experimental-modules --es-module-specifier-resolution=node" dist/index.js
+export NODE_ENV=production && pm2 start --node-args="--experimental-modules --es-module-specifier-resolution=node" dist/index.js
