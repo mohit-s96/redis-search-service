a3517d57173661e0c654afebb2cdaeab7532be0e
Mohit Srivastava <mohitsrivastava67@gmail.com>
2022-10-08 17:06:04 +0530

    #1: return actual ip instead of localhost

--- src/routes/home/controller.ts
+++ src/routes/home/controller.ts
@@ -1,5 +1,10 @@
 import { Response, Request } from "express";
 
 export default (req: Request, res: Response) => {
-  res.send("Your ip is " + req.ip);
+  const forwarded = req.headers["x-forwarded-for"];
+  let ip: string;
+  if (typeof forwarded === "string") {
+    ip = forwarded ? forwarded.split(/, /)[0] : req.socket.remoteAddress;
+  }
+  res.send("Your ip is " + ip || req.socket.remoteAddress);
 };
